07cb4cd487f83737625427a6b6e3d066f28a0a3e
deluwarhoisn <babuhossen301@gmail.com>
2025-08-05 23:53:03 +0600

    sort and gushano

---
+++ sort.js
@@ -0,0 +1,16 @@
+const numbers =[6,5,8,9,3,4];
+// console.log(numbers);
+numbers.sort();
+// console.log(numbers);
+
+const friends = ['sakib','nokib','rakib','bakibulla']
+console.log(friends);
+friends.sort();
+// console.log(friends);
+
+const ages = [1, 5, 100, 8, 2, 28]
+// console.log(ages);
+// ages.sort();
+// console.log(ages);
+const sorted_ages = ages.sort(function (a,b) {return a-b})
+console.log(sorted_ages);
