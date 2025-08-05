05b50473657b428db235d8a7c4e60d9aaed143af
deluwarhoisn <babuhossen301@gmail.com>
2025-08-05 22:53:41 +0600

    while loop

---
+++ array-looping.js
@@ -0,0 +1,27 @@
+const fruits = ['apple','Banana','anur','watermelon','daav']
+// console.log(fruits[2]);
+
+
+for(let i =0; i <fruits.length; i++){
+    console.log(i);
+    console.log(fruits[i])
+}
+
+
+const number = [12,45,78,145,7,44,765];
+for(let j =0; j<number.length; j++){
+    console.log(number[j]);
+}
+
+let k = 0;
+while(k < fruits.length){
+    console.log(fruits[k]);
+    k++;
+}
+
+const price = [,25,13,29,56,];
+let n =0;
+while(n <price.length){
+    console.log(price[n])
+    n++
+}
