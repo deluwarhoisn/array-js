e664af0e8cb56b6ade890e929b737d13ff085b1a
deluwarhoisn <babuhossen301@gmail.com>
2025-08-05 23:34:49 +0600

    reversh

--- revarsh.js
+++ revarsh.js
@@ -7,4 +7,13 @@ for(let i =0;i<number.length;i++){
     reversed.unshift(number[i]);
 }
 
+for(const num of number){
+    reversed.push(num);
+}
+
+console.log(number);
+const result =number.reverse();
+console.log(number);
+console.log(result);
 console.log(reversed)
+
