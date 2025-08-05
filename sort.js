86314a9ddca4f4122e3972cd009146d188dafa1a
deluwarhoisn <babuhossen301@gmail.com>
2025-08-05 23:56:14 +0600

    commit last

--- sort.js
+++ sort.js
@@ -15,4 +15,4 @@ const ages = [1, 5, 100, 8, 2, 28]
 const sorted_ages = ages.sort(function (a,b) {return a-b})
 console.log(sorted_ages);
 const sorted_ages_desc = ages.sort(function (a,b) {return b-a})
-console.log(sorted_ages);
+console.log(sorted_ages_desc);
