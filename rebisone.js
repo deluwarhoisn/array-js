d528b6a7825ead824f75359f84d2e65e64e92687
deluwarhoisn <babuhossen301@gmail.com>
2025-08-05 21:49:57 +0600

    rebishon.javascript

---
+++ rebisone.js
@@ -0,0 +1,45 @@
+/**
+ * 1.const --> value not going to change
+ * 2.let --> value might change
+ * 3.var --> should not use (unless exceptional cases)
+ */
+
+const price = 200;  // number type
+
+const name = 'babu hosin';  // string type
+ const ispoor = false ;  // boolean
+
+ const friends = ['elion', 'milon','billa'] //array
+ const student ={ class: 9, name: 'tomij', age:17, addresh:'bangladesh'};
+
+ //condition
+
+ if(price> 1000){
+    console.log('to expanceiv. not for me')
+ }
+ else if(price > 200){
+    console.log('moddobitto')
+ }
+ else(
+    console.log('my bujet oky')
+
+ )
+
+ //loop
+
+ let pushup = 0;
+ while(pushup < 15){
+    console.log('pushup')
+    pushup++;
+ }
+
+ let start = 0;
+ while(start <15){
+   console.log('raning mane' ,start)
+   start++;
+ }
+
+ //for
+ for (let i =0; i<10; i++){
+   console.log(i)
+ }
