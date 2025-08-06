ebc56ad543b29a22eec3ac19b8f78aa66347a9d4
deluwarhoisn <babuhossen301@gmail.com>
2025-08-06 20:07:19 +0600

    object-array

--- object-array.js
+++ object-array.js
@@ -1,5 +1,8 @@
 const numbers = [1,2,43,54,4]
 const players = [{},{},{},];
-const employees = {
-    {name:'Ashik',designation:'content writer', }
-}
+const employees = [
+    { name:'Ashik',designation:'content writer', salary: 25000 },
+    { name: 'atik', designation: 'Devoloper', salary:29000 },
+    { name:'abdulrahim',designation:'Digital markating', salary:21000 },
+]
+console.log(employees[2].designation);
