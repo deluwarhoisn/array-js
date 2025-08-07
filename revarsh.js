a9352ab1f54099d8a6e9ecb9f7b7d26876227961
deluwarhoisn <babuhossen301@gmail.com>
2025-08-07 13:26:15 +0600

    task-1

--- revarsh.js
+++ revarsh.js
@@ -1,19 +1,28 @@
-const number = [1,2,3,4,5];
+// const number = [1,2,3,4,5];
 
 
-const reversed =[];
-for(let i =0;i<number.length;i++){
-    console.log(number[i]);
-    reversed.unshift(number[i]);
+// const reversed =[];
+// for(let i =0;i<number.length;i++){
+//     console.log(number[i]);
+//     reversed.unshift(number[i]);
+// }
+
+// for(const num of number){
+//     reversed.push(num);
+// }
+
+// console.log(number);
+// const result =number.reverse();
+// console.log(number);
+// console.log(result);
+// console.log(reversed)
+
+
+
+const colors = ['red', 'blue', 'green', 'yellow', 'orange'];
+
+let reversed =[];
+for(const color of colors){
+    reversed.unshift(color);
 }
-
-for(const num of number){
-    reversed.push(num);
-}
-
-console.log(number);
-const result =number.reverse();
-console.log(number);
-console.log(result);
-console.log(reversed)
-
+console.log(reversed);
